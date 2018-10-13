df05a711b5709a5437d2a6b8d1f89ff5be74f5fb
Mehmet Ali Peker <maps6134@hotmail.com>
2018-10-14 01:14:48 +0300

    .

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const request = require('request');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const moment = require('moment');
+
 
 mongoose.connect('mongodb://pisano:KbE8MdbLfCmr2GGJjCW7zT6njYZtmcRn@52.28.78.239:27017/pisano')
     .then((err) => {
@@ -42,104 +42,95 @@ let ProductSchema = new Schema({
 });
 
 let Product = mongoose.model('Product', ProductSchema);
-/*
+
 let CrawlSchema = new Schema({
     url: String,
     time: Date
 });
 
-let Crawl = mongoose.model('Crawl', CrawlSchema);
-*/
+let Crawl = mongoose.model('Crawl', ProductSchema);
+
 let obselete = []; // Array of what was crawled already
 
 let c = new Crawler();
 
 function crawlAllUrls(url) {
-    console.log(`Crawling ${url}`);
-    /*
-    Crawl.findOne({ url, time: { $gte: moment().startOf('day').toDate() } }, (err, doc) => {
-        if(err) throw err;
-        if(!doc) {
-        */
-            c.direct({
-                uri: url,
-                callback: function (err, res, done) {
-                    if (err) throw err;
-                    let $ = res.$;
-                    try {
-                        let urls = $("a");
-                        let product = $('#urun.urun1');
-                        if(product.html() != null) {
-                            let name = $('#ozet .row .baslik h1 a');
-                            let title = name.attr('title');
-                            let model = cheerio.load(name.text().replace(title, '')).text().match(/\((.*?)\)/);
-                            model = model == null ? '' : model[1];
-                            let image = product.find('#resim .buyuk div a img').attr('src');
-                            let prices = $('#fiyatlar .fiyatlar .fiyat').children();
-                            let sellers = [];
-                            let rate = $('.uyepuani div .rating .basic').attr('data-average');
-                            let features = $('#ozellikler').html();
-                            for(let i = 0; i < prices.length; i++) {
-                                let price = prices[i];
-                                let seller = {};
-                                seller.seller = price.attribs.title.split(title)[0].trim();
-                                let priceDOM = $(price);
-                                seller.title = priceDOM.find('.urun_adi')[0].children[0].data.trim();
-                                seller.price = priceDOM.find('.urun_fiyat')[0].attribs["data-sort"];
-                                seller.price = seller.price.substring(0, seller.price.length - 2);
-                                seller.shipment = priceDOM.find('.urun_fiyat p strong span').text() === 'Ücretsiz Kargo' ? 'free' : 'paid';
-                                seller.url = price.attribs.href;
-                                let r = request(seller.url, function (e, response) {
-                                    seller.url = typeof response !== 'undefined' ? response.request.uri.href : seller.url;
-                                    sellers.push(seller);
+    //console.log(`Crawling ${url}`);
+
+    c.queue({
+        uri: url,
+        callback: function (err, res, done) {
+            if (err) throw err;
+            let $ = res.$;
+            try {
+                let urls = $("a");
+                let product = $('#urun.urun1');
+                if(product.html() != null) {
+                    let name = $('#ozet .row .baslik h1 a');
+                    let title = name.attr('title');
+                    let model = cheerio.load(name.text().replace(title, '')).text().match(/\((.*?)\)/);
+                    model = model == null ? '' : model[1];
+                    let image = product.find('#resim .buyuk div a img').attr('src');
+                    let prices = $('#fiyatlar .fiyatlar .fiyat').children();
+                    let sellers = [];
+                    let rate = $('.uyepuani div .rating .basic').attr('data-average');
+                    for(let i = 0; i < prices.length; i++) {
+                        let price = prices[i];
+                        let seller = {};
+                        seller.seller = price.attribs.title.split(title)[0].trim();
+                        let priceDOM = $(price);
+                        seller.title = priceDOM.find('.urun_adi')[0].children[0].data.trim();
+                        seller.price = priceDOM.find('.urun_fiyat')[0].attribs["data-sort"];
+                        seller.price = seller.price.substring(0, seller.price.length - 2);
+                        seller.shipment = priceDOM.find('.urun_fiyat p strong span').text() === 'Ücretsiz Kargo' ? 'free' : 'paid';
+                        seller.url = price.attribs.href;
+                        let r = request(seller.url, function (e, response) {
+                            seller.url = typeof response !== 'undefined' ? response.request.uri.href : seller.url;
+                            sellers.push(seller);
+                        });
+
+                        if(i == prices.length -1) {
+                            setTimeout(() => {
+                                productObj = { title, model, rate, image, sellers };
+                                let options = { upsert: true, new: true, setDefaultsOnInsert: true };
+                                new Product(productObj);
+                                Product.findOneAndUpdate({ title, model }, productObj, options, function(error, result) {
+                                    if (error) return;
+                                    console.log(result);
+                                    // do something with the document
                                 });
 
-                                if(i == prices.length -1) {
-                                    setTimeout(() => {
-                                        productObj = { title, model, rate, image, sellers, features };
-                                        let options = { upsert: true, new: true, setDefaultsOnInsert: true };
-                                        new Product(productObj);
-                                        Product.findOneAndUpdate({ title, model }, productObj, options, function(error, result) {
-                                            if (error) return;
-                                            console.log(result);
-                                            // do something with the document
-                                        });
-
-                                        new Crawl({ url: url, time: Date.now() }).save();
-                                    }, 3000);
-                                }
-                            }
-
-                            console.log('P: ', title, '-', model);
+                                new Crawl({ url: url, time: Date.now() }).save();
+                            }, 3000);
                         }
-
-                        Object.keys(urls).forEach((item) => {
-                            if (urls[item].type === 'tag') {
-                                let href = urls[item].attribs.href;
-                                if (href && !obselete.includes(href)) {
-                                    href = href.trim();
-                                    obselete.push(href);
-                                    // Slow down the
-                                    setTimeout(function() {
-                                        href.startsWith('http') ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`) // The latter might need extra code to test if its the same site and it is a full domain with no URI
-                                    }, 10000)
-
-                                }
-                            }
-                        });
-                    } catch (e) {
-                        throw e;
-                        console.error(`Encountered an error crawling ${url}. Aborting crawl.`);
-                        done()
-
                     }
-                    done();
+
+                    console.log('P: ', title, '-', model);
                 }
-            })
-    /*
+
+                Object.keys(urls).forEach((item) => {
+                    if (urls[item].type === 'tag') {
+                        let href = urls[item].attribs.href;
+                        if (href && !obselete.includes(href)) {
+                            href = href.trim();
+                            obselete.push(href);
+                            // Slow down the
+                            setTimeout(function() {
+                                href.startsWith('http') ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`) // The latter might need extra code to test if its the same site and it is a full domain with no URI
+                            }, 10000)
+
+                        }
+                    }
+                });
+            } catch (e) {
+                throw e;
+                console.error(`Encountered an error crawling ${url}. Aborting crawl.`);
+                done()
+
+            }
+            done();
         }
-    });
-    */
+    })
 }
 
 crawlAllUrls(process.argv[2]);
