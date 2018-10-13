8d107d1e2a2f9632d6274fd99a78b60860af666b
Mehmet Ali Peker <maps6134@hotmail.com>
2018-10-14 01:31:32 +0300

    .....

--- app.js
+++ app.js
@@ -64,6 +64,18 @@ function crawlAllUrls(url) {
             let $ = res.$;
             try {
                 let urls = $("a");
+
+                let product = $('#urun.urun1');
+                if(product.html() != null) {
+                    let crawlObj = {url: href, time: moment().startOf('day').toDate()};
+                    let options = {upsert: true, new: true, setDefaultsOnInsert: true};
+                    Crawl.findOneAndUpdate({url}, crawlObj, options, function (error, result) {
+                        if (error) return;
+                        console.log(result);
+                        // do something with the document
+                    });
+                }
+
                 /*let product = $('#urun.urun1');
                 if(product.html() != null) {
                     let name = $('#ozet .row .baslik h1 a');
@@ -113,16 +125,6 @@ function crawlAllUrls(url) {
                         if (href && !obselete.includes(href)) {
                             href = href.trim();
                             obselete.push(href);
-                            let product = $('#urun.urun1');
-                            if(product.html() != null) {
-                                crawlObj = {url: href, time: moment().startOf('day').toDate()};
-                                let options = {upsert: true, new: true, setDefaultsOnInsert: true};
-                                Crawl.findOneAndUpdate({url}, crawlObj, options, function (error, result) {
-                                    if (error) return;
-                                    console.log(result);
-                                    // do something with the document
-                                });
-                            }
                             // Slow down the
                             setTimeout(function() {
                                 href.startsWith('http') ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`) // The latter might need extra code to test if its the same site and it is a full domain with no URI
